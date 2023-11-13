32094034a3aa0771004090426591c06b0a3a5187
Colin Willits <24CWillits@wpss.org>
2023-11-13 15:52:50 -0500

    Luke Messed Up

--- A.py
+++ A.py
@@ -2,12 +2,12 @@
 # you aren't going to know what they are doing until after they push their changes
 # if you need a refresher of what a madlib is, go here: https://www.madlibs.com/MadLibs-Teachers-Guide-nocrops.pdf
 
-year_in_the_future = 0 # assign numbers or strings to the end of these versions
-wacky_robot_name = ""
-adjective_1 = ""
-violent_verb = ""
-tech_company = ""
-dollar_amount = ""
-adjective_2 = ""
-noun_1 = ""
-large_store = ""
+year_in_the_future = 3002 # assign numbers or strings to the end of these versions
+wacky_robot_name = "Luke"
+adjective_1 = "bigly"
+violent_verb = "violates"
+tech_company = "Apple"
+dollar_amount = "$1000000000000000"
+adjective_2 = "smallly"
+noun_1 = "person"
+large_store = "Walmart"
