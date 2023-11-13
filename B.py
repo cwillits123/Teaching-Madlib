636eb77502fa2d4054876f54cfa25d558812d995
Colin Willits <24CWillits@wpss.org>
2023-11-13 15:56:05 -0500

    You stupid

--- B.py
+++ B.py
@@ -2,12 +2,12 @@
 # you aren't going to know what they are doing until after they push their changes
 # if you need a refresher of what a madlib is, go here: https://www.madlibs.com/MadLibs-Teachers-Guide-nocrops.pdf
 
-number_between_1_and_9000 = 0 # assign some number or string at the end of each of these lines
-adjective_1 = ""
-noun_1 = ""
-famous_person = ""
-adjective_2 = ""
-very_large_number = 0
-noun_2 = ""
-adjective_2 = ""
-large_store = ""
+number_between_1_and_9000 = 9000 # assign some number or string at the end of each of these lines
+adjective_1 = "greatly"
+noun_1 = "job"
+famous_person = "Luke Maxwell"
+adjective_2 = "hugely"
+very_large_number = 100020200304
+noun_2 = "elephant"
+adjective_2 = "hugely"
+large_store = "Target"
